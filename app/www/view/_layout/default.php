a1def49469320040957723a7ecda9a523e1aa17e
chloe2013 <chloeye13@gmail.com>
2013-06-25 17:22:17 +0800

    temp

--- app/www/view/_layout/default.php
+++ app/www/view/_layout/default.php
@@ -13,8 +13,7 @@
 		<link rel="shortcut icon" href="/favicon.ico" />
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 		<link href="http://static.{config.domain}/css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-		<script src="http://static.{config.domain}/js/bootstrap.min.js"></script>
+		
 	</head>
 	<body>
 		<div>
@@ -35,5 +34,8 @@
 				</p>
 			</footer>
 		</div>
+		
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script src="http://static.{config.domain}/js/bootstrap.min.js"></script>
 	</body>
 </html>
---
+++ merge.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# 合并css
+cd /home/www/yeap/app/static/css/source
+#
+cat bootstrap-cerulean.css bootstrap-responsive.css > ../merge/bootstrap.css
+#
+cat charisma-app.css fullcalendar.css fullcalendar.print.css chosen.css uniform.default.css colorbox.css jquery.cleditor.css jquery.noty.css noty_theme_default.css elfinder.min.css elfinder.theme.css jquery.iphone.toggle.css opa-icons.css uploadify.css > ../merge/plugin.css
+
+cat jquery-ui-1.8.21.custom.css > ../merge/jquery-ui.css
+
+cd ../merge
+find -name "*.css" -exec 'cat' {} \; > ../main.css
+
+# 合并js
+cd /home/www/yeap/app/static/js/source
+cat bootstrap-transition.js bootstrap-alert.js bootstrap-modal.js bootstrap-dropdown.js bootstrap-scrollspy.js bootstrap-tab.js bootstrap-tooltip.js bootstrap-popover.js bootstrap-button.js bootstrap-collapse.js bootstrap-carousel.js bootstrap-typeahead.js bootstrap-tour.js> ../merge/bootstrap.js
+
+#
+cat jquery.cookie.js jquery.dataTables.min.js fullcalendar.min.js jquery.chosen.min.js jquery.uniform.min.js jquery.colorbox.min.js jquery.cleditor.min.js jquery.noty.js jquery.elfinder.min.js jquery.raty.min.js jquery.iphone.toggle.js jquery.autogrow-textarea.js jquery.uploadify-3.1.min.js jquery.history.js > ../merge/plugin.js
+
+#
+cat excanvas.js jquery.flot.min.js jquery.flot.pie.min.js jquery.flot.stack.js jquery.flot.resize.min.js  > ../merge/chart.js
+
+cat charisma.js > ../merge/charisma.js
+
+cat jquery-1.7.2.min.js > ../merge/jquery.min.js
+
+cat jquery-ui-1.8.21.custom.min.js > ../merge/jquery-ui.custom.min.js
+
+cd ../merge
+find -name "*.js" -exec 'cat' {} \; > ../main.js
+
